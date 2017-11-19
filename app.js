9ad0003e1b8c144287fdd54047a491e8af9c3f3d
Jennie Robinson Faber <jennie@jenniefaber.com>
2017-11-19 15:24:29 -0500

    Squashed commit of the following:
    commit 3e8aaff13e47c00cd450c62f79fafb12525135a8
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Sun Nov 19 15:23:56 2017 -0500
    
        New views and includes
    
    commit 5878032f084f70812f7aa0d8b290877a8aa0b4fa
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Sun Nov 19 12:29:04 2017 -0500
    
        Layout updates
    
    commit d18770934b06e3d3b87b35c23dc201b51fbf30e2
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Sat Nov 18 15:28:14 2017 -0500
    
        Model updates
    
    commit 3d33192ee3e57fc699c38b35d1ecd92fd9640f4f
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Fri Nov 17 19:11:28 2017 -0500
    
        App.js updates for articles and events
    
    commit edce0afce996844646b83b49784ad2af07fb809b
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Tue Nov 14 17:43:25 2017 -0500
    
        Layout tweaks
    
    commit 3ca83b1e5dc4f012a55342472e29615118bfc9d5
    Merge: 78a46ac bcb169e
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Mon Nov 13 15:13:24 2017 -0500
    
        Merge branch 'datocms' of github.com:jennie/pangaea-g into datocms
    
        # Conflicts:
        #	app.js
    
    commit 78a46ac089d4463dd4cef6bd52419cce6aa50458
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Mon Nov 13 15:12:24 2017 -0500
    
        Fix matchers
    
    commit bcb169ec201139bbc09332e4c5a1e21b4ccc7dfe
    Author: jennie <jennie@dmg.to>
    Date:   Mon Nov 13 13:41:07 2017 -0500
    
        Css updates
    
    commit a61b321b2918026d3097f0f21c50104f1e2eabe0
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Mon Nov 13 00:16:17 2017 -0500
    
        Cleaning up pipeline
    
    commit e3f382f38ef05c1a71b11f4631131a3ceb4a0037
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Sun Nov 12 15:52:40 2017 -0500
    
        app updates
    
    commit 5ceb58fc19ec8a89e547dc628e0826c00605d6a7
    Author: Jennie Robinson Faber <jennie@jenniefaber.com>
    Date:   Sat Nov 11 17:49:54 2017 -0500
    
        Interim config

--- app.js
+++ app.js
@@ -1,90 +1,125 @@
-const Collections = require('spike-collections')
-const Records = require('spike-records')
-const path = require('path')
-const MarkdownIt = require('markdown-it')
-const markdownitFootnote = require('markdown-it-footnote')
-const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
-const jsStandards = require('spike-js-standards')
-const pageId = require('spike-page-id')
-const sugarml = require('sugarml')
-const sugarss = require('sugarss')
-const df = require('dateformat')
+const env                     = require('dotenv').config()
+const path                    = require('path')
+const htmlStandards           = require('reshape-standard')
 
-const env = process.env.api_key
+// const styleGuide              = require('postcss-style-guide')
+const cssStandards            = require('spike-css-standards')
 
-const md = new MarkdownIt().use(markdownitFootnote)
-const locals = {
-  md: md.render.bind(md),
-}
+const jsStandards             = require('spike-js-standards')
+const pageId                  = require('spike-page-id')
+const sugarml                 = require('sugarml')
+const sugarss                 = require('sugarss')
+const df                      = require('dateformat')
+const fn                      = require('format-num')
+const SpikeDatoCMS            = require('spike-datocms')
+const MarkdownIt              = require('markdown-it')
+const markdownitFootnote      = require('markdown-it-footnote')
+const markdownItTocAndAnchor  = require('markdown-it-toc-and-anchor').default
+const markdownItAttrs         = require('markdown-it-attrs')
+const markdownItContainer     = require('markdown-it-container')
+const markdownItSup           = require('markdown-it-sup')
+const markdown                = new MarkdownIt().use(markdownItTocAndAnchor, { anchorLink: false, tocFirstLevel: 3 })
 
+const locals                  = { }
 
-const apiUrl = 'https://api.graph.cool/simple/v1/cj9frf64r26kn0129ol67c71f'
-
-const collections = new Collections({
+const datos = new SpikeDatoCMS({
   addDataTo: locals,
-  collections: {
-    pages: {
-      files: 'collections/pages/**',
-      markdownLayout: 'views/__page_template.sgr',
-      permalinks: (p) => {
-        const m = p.match(/^.*?\/collections\/pages\/(.*?)\./)
-        console.log(`${m[1]}`)
-        return `${m[1]}.html`
-      }
-    }
-    // reports: {
-    //   files: 'collections/reports/**',
-    //   markdownLayout: 'templates/post.sgr',
-    //   permalinks: (p) => {
-    //     const m = p.match(/^.*?\/reports\/(.*?)\./)
-    //     // console.log(`${m[1]}`)
-    //     return `${m[1]}.html`
-    //   },
-    //   transform: (data) => {
-    //     const d = new Date(data.date)
-    //     data.newdate = df(d, "mmmm yyyy")
-    //     return data
-    //   }
-    // }
-  }
-})
-
-const records = new Records({
-  addDataTo: locals,
-  posts: {
-    graphql: {
-      url: apiUrl,
-      headers: { Authorization: 'Bearer ' + process.env.api_key},
-      query: `{
-        allQuotes {
-          attribution, text
-        }
-      }`
-    },
-    transform: (res) => res.data.allQuotes,
+  token: process.env.dato_api_key,
+  models: [
+  { name: 'quote' },
+  {
+    name: 'person',
     template: {
-      path: 'views/quote.sgr',
-      output: (post) => `quote/${post.postSlug}.html`
+      path: 'views/_person.sgr',
+      output: (person) => { return `profile/${person.slug}.html` }
     }
 
-  }
+  },
+  {
+    name: 'article',
+    template: {
+      path: 'views/_article.sgr',
+      output: (article) => { return `blog/${article.slug}.html` }
+    },
+    json: 'articles.json'
+  },
+  {
+    name: 'event',
+    transform: (data) => {
+      if (data.tickets) {
+        const tickets = data.tickets
+        tickets.forEach(function(ticket, index) {
+          tax = ticket.price * ticket.taxRate
+          total = ticket.price + tax
+          ticket.total = total
+        })
+      }
+      return data
+    },
+    template: {
+      path: 'views/_event.sgr',
+      output: (event) => { return `events/${event.slug}.html` }
+    }
+  },
+  {
+    name: 'home_page',
+    template: {
+      path: 'views/_home_page.sgr',
+      output: (page) => { return `/index.html` }
+    }
+  },
+  {
+    name: 'page',
+    template: {
+      path: 'views/_page.sgr',
+      output: (page) => {
+        // TODO: this is a bit precarious – don't use if nesting goes more
+        // than one level deep. Refactor.
+        if (page.parentId) { return `/${page.parentId.slug}/${page.slug}.html` }
+        else { return `/${page.slug}.html` }
+      }
+    },
+    transform: (data) => {
+      if (data.parentId === null) {
+        data.overview = true
+      }
+      if (data.date) {
+        const d = new Date(data.date)
+        data.newdate = df(d, "mmmm yyyy")
+      }
+      // add toc to the local {{ item }} if toc is true
+      if (data.toc == true) {
+        markdown.render(data.body, {
+          tocCallback: function(tocMarkdown, tocArray, tocHtml) {
+            data.toc_content = tocHtml
+          }
+        })
+      }
+      return data
+    }
+  }]
 })
+
+
 
 module.exports = {
   devtool: 'source-map',
-  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
-  ignore: ['**/layout.sgr', '**/__post_template.sgr', '**/__page_template.sgr', '**/.*', 'readme.md', 'yarn.lock', 'custom_modules/**'],
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.css' },
+  ignore: [ '**/layout.sgr', '**/.*', 'readme.md', 'yarn.lock', 'custom_modules/**' ],
   reshape: htmlStandards({
     parser: sugarml,
-    locals: (ctx) => { return collections.locals(ctx, Object.assign({ pageId: pageId(ctx) }, locals)) },
-    minify: env === 'production'
+    locals: { df: df.bind(df), fn: fn.bind(fn), md: markdown.render.bind(markdown)},
+    markdownPlugins: [ markdownitFootnote, markdownItAttrs, markdownItContainer, markdownItSup ],
+    retext: { quotes: false }
   }),
   postcss: cssStandards({
-    parser: sugarss,
-    minify: env === 'production',
-    warnForDuplicates: env !== 'production'
+    locals: { datos }
+    // ,
+    // appendPlugins: styleGuide({
+    //   project: 'Pangaea 2.0',
+    //   dest: 'public/styleguide/index.html'
+    // })
   }),
   babel: jsStandards(),
-  plugins: [collections, records]
+  plugins: [datos]
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,60 @@
-// silence is golden
+//
+// // Set your secret key: remember to change this to your live secret key in production
+// // See your keys here: https://dashboard.stripe.com/account/apikeys
+// // var stripe = require("stripe")(process.env.test_stripe_public_key);
+//
+// // Token is created using Checkout or Elements!
+// // Get the payment token ID submitted by the form:
+// var token = request.body.stripeToken; // Using Express
+//
+// // Charge the user's card:
+// stripe.charges.create({
+//   amount: 1000,
+//   currency: "cad",
+//   description: "Example charge",
+//   source: token,
+// }, function(err, charge) {
+//   // asynchronously called
+// });
+
+// var requestURL = '/articles.json';
+// var request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+//
+// request.responseType = 'json';
+// request.send();
+// request.onload = function() {
+//   const articles = request.response;
+//   // console.log(articles);
+//   // showArticles(articles);
+//   getAuthorsArticles(articles, "129390")
+// }
+//
+// function showArticles(postIds) {
+//   // console.log(postIds)
+//   var posts = postIds;
+//   for (var i = 0; i < posts.length; i++) {
+//     console.log(posts[i].title)
+//   }
+// }
+//
+// function getAuthorsArticles(jsonObj, author) {
+//   var posts = jsonObj;
+//
+//   for (var i = 0; i < posts.length; i++) {
+//     var authors = posts[i].authors
+//     var authorIds = new Array;
+//     var authorPosts = new Array;
+//
+//     authors.forEach(function(names) {
+//       authorIds.push(names.id)
+//     });
+//
+//     if(authorIds.includes(author)) {
+//       authorPosts.push(posts[i])
+//     }
+//
+//     showArticles(authorPosts);
+//
+//   }
+// }
